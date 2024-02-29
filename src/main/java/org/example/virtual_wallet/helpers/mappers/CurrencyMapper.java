4bb0e6771a9edf51f7ed74b0d0a0ad008322c1e7
Georgi Valchanov <valchanov.biz@gmail.com>
2024-02-29 13:28:05 +0200

    Currency utilities added.

---
+++ src/main/java/org/example/virtual_wallet/helpers/mappers/CurrencyMapper.java
@@ -0,0 +1,21 @@
+package org.example.virtual_wallet.helpers.mappers;
+
+import org.example.virtual_wallet.models.Currency;
+import org.example.virtual_wallet.models.dtos.CurrencyDto;
+import org.example.virtual_wallet.services.contracts.CurrencyService;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CurrencyMapper {
+private final CurrencyService currencyService;
+
+    public CurrencyMapper(CurrencyService currencyService) {
+        this.currencyService = currencyService;
+    }
+
+    public Currency currencyDto (CurrencyDto currencyDto){
+        Currency currency = new Currency();
+        currency.setCurrency(currencyDto.getAbbreviation());
+        return currency;
+    }
+}
