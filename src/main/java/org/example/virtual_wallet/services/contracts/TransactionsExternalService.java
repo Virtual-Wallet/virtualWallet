765d3e25aba02e04eba8f60d9af367a1bdaa04a2
iggydob <138367360+iggydob@users.noreply.github.com>
2024-03-05 15:43:08 +0200

    TransactionExternal service interface added

---
+++ src/main/java/org/example/virtual_wallet/services/contracts/TransactionsExternalService.java
@@ -0,0 +1,16 @@
+package org.example.virtual_wallet.services.contracts;
+
+import org.example.virtual_wallet.models.TransactionsExternal;
+import org.example.virtual_wallet.models.User;
+
+import java.util.List;
+
+public interface TransactionsExternalService {
+    public TransactionsExternal create(TransactionsExternal transactionsExternal);
+
+    List<TransactionsExternal> getAll();
+
+    List<TransactionsExternal> getDeposits(User user);
+
+    List<TransactionsExternal> getWithdrawals(User user);
+}
